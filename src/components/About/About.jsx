9ce7713d742bd2ba363c49fa36914e94ecda1cda
harish-govindasamy <harish.cs18@bitsathy.ac.in>
2025-03-10 05:08:52 +0530

    commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./About.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import about_profile from "../../assets/about_profile.svg";
+
+const About = () => {
+  return (
+    <div id="about" className="about">
+      <div className="about-title">
+        <h1>About me</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+          <img src={about_profile} alt="" />
+        </div>
+        <div className="about-right">
+          <div className="about-para">
+            <p>
+              I’m a Frontend Developer from Tamil Nadu, India, with a deep
+              passion for crafting beautiful and functional web experiences.
+              Ever since I started coding, I’ve been fascinated by how design
+              and technology come together to create seamless digital
+              interactions.
+            </p>
+            <p>
+              I love working on projects that challenge me to think creatively
+              and push boundaries. Whether it's designing a smooth, responsive
+              UI or optimizing performance for a flawless user experience, I’m
+              always excited to dive in.
+            </p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill">
+              <p>HTML & CSS</p> <hr style={{ width: "50%" }} />
+            </div>
+            <div className="about-skill">
+              <p>JavaScript</p> <hr style={{ width: "70%" }} />
+            </div>
+            <div className="about-skill">
+              <p>React JS</p> <hr style={{ width: "60%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Next JS</p> <hr style={{ width: "50%" }} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>30+</h1>
+          <p>TASKS COMPLETED</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>10+</h1>
+          <p>PROJECTS COMPLETED</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>15+</h1>
+          <p>HAPPY CLIENTS</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default About;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Hero.css";
+import profile_img from "../../assets/profile_img.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+const Hero = () => {
+  return (
+    <div id="home" className="hero">
+      <img src={profile_img} alt="" />
+      <h1>
+        <span>I'm Harish Govindasamy,</span> Frontend Developer based in India.
+      </h1>
+      <p>
+        I'm a Passionate Frontend Developer from Tamil Nadu, India, with a
+        strong foundation in web technologies. Eager to apply my skills in
+        designing and developing user-friendly, responsive, and visually
+        appealing web applications. Seeking an opportunity to contribute
+        innovative solutions while continuously learning and growing in a
+        dynamic work environment.
+      </p>
+
+      <div className="hero-action">
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect With Me
+          </AnchorLink>
+        </div>
+        <div className="hero-resume">My Resume</div>
+      </div>
+    </div>
+  );
+};
+export default Hero;
